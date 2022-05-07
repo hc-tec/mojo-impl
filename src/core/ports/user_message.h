a995809ef45aaeb5e6ceb07c9c7d04391144b26a
hc-tec <2173324540@qq.com>
2022-05-07 14:17:03 +0800

    feat: incre interface for protocols

--- src/core/ports/user_message.h
+++ src/core/ports/user_message.h
@@ -5,6 +5,7 @@
 #ifndef MOJO_IMPL_USER_MESSAGE_H
 #define MOJO_IMPL_USER_MESSAGE_H
 
+#include "core/protocol.h"
 #include "core/byte_array.h"
 #include "core/ports/name.h"
 
@@ -12,14 +13,33 @@ namespace tit {
 namespace mojo {
 namespace ports {
 
-class UserMessage {
+class UserMessage : public ProtocolInterface {
  public:
   using Ptr = std::shared_ptr<UserMessage>;
+
+  static Ptr Create() {
+    return std::make_shared<UserMessage>();
+  }
+
+  static Ptr Create(const PortName& port_name, const std::string& data) {
+    Ptr message = std::make_shared<UserMessage>();
+    message->set_port_name(port_name);
+    message->set_data(data);
+    return message;
+  }
 
   PortName port_name() const { return port_name_; }
   std::string data() const { return data_; }
 
-  ByteArray::Ptr Encode() {
+  void set_port_name(const PortName& port_name) {
+    port_name_ = port_name;
+  }
+
+  void set_data(const std::string& data) {
+    data_ = data;
+  }
+
+  ByteArray::Ptr Encode() override {
     ByteArray::Ptr bt = std::make_shared<ByteArray>();
     bt->writeFuint64(port_name_.v1);
     bt->writeFuint64(port_name_.v2);
@@ -28,13 +48,22 @@ class UserMessage {
     return bt;
   }
 
-  void Decode(const ByteArray::Ptr& bt) {
+  void Decode(const ByteArray::Ptr& bt) override {
     uint64_t v1, v2;
     v1 = bt->readFuint64();
     v2 = bt->readFuint64();
     port_name_ = PortName(v1, v2);
     data_ = bt->readStringF32();
   }
+
+  std::string ToString() {
+    std::stringstream ss;
+    ss << "[ port_name_: " << "v1=" << port_name_.v1
+       << " v2=" << port_name_.v2 << "\ndata=" << data_
+       << "]";
+    return ss.str();
+  }
+
  private:
   PortName port_name_ { kInvalidPortName };
   std::string data_;
--- src/core/protocol.h
+++ src/core/protocol.h
@@ -14,19 +14,30 @@
 namespace tit {
 namespace mojo {
 
+class ProtocolInterface {
+  // Encode object into ByteArray, then it can transform to string
+  // used to transfer in network
+  virtual ByteArray::Ptr Encode() = 0;
+  // Decode the ByteArray into Protocol,
+  // then we can use the decoded data received
+  virtual void Decode(const ByteArray::Ptr& bt) = 0;
+};
+
 // private protocol for mojo ipc
 // 1st byte is magic number
 // 2nd byte is version number
 // 3rd byte is message type
 // 4 bytes from 4th to 8th is content length
 // Other bytes is data
-class Protocol {
+class Protocol : public ProtocolInterface {
  public:
   using Ptr = std::shared_ptr<Protocol>;
 
   static constexpr uint8_t kMagic = 0xcc;
   static constexpr uint8_t kDefaultVersion = 0x01;
-  static constexpr uint8_t kBaseLen = 7;
+  // 7: header size
+  // 4: used to represent data length
+  static constexpr uint8_t kBaseLen = 7 + 4;
 
   enum class MsgType : uint8_t {
     kNormal,
@@ -35,7 +46,11 @@ class Protocol {
     kUpgradeReject
   };
 
-  static Protocol::Ptr Create(uint8_t type, const std::string& content) {
+  static Protocol::Ptr Create() {
+    return std::make_shared<Protocol>();
+  }
+
+  static Protocol::Ptr Create(MsgType type, const std::string& content) {
     Protocol::Ptr proto = std::make_shared<Protocol>();
     proto->set_msg_type(type);
     proto->set_content(content);
@@ -50,7 +65,7 @@ class Protocol {
 
   void set_magic(uint8_t magic) { magic_ = magic; }
   void set_version(uint8_t version) { version_ = version; }
-  void set_msg_type(uint8_t type) { type_ = static_cast<uint8_t>(type); }
+  void set_msg_type(MsgType type) { type_ = static_cast<uint8_t>(type); }
   void set_content_length(uint32_t len) { content_length_ = len; }
   void set_content(const std::string& content) { data_ = content; }
 
@@ -64,29 +79,31 @@ class Protocol {
     return bt;
   }
 
-  ByteArray::Ptr Encode() {
+  ByteArray::Ptr Encode() override {
     ByteArray::Ptr bt = std::make_shared<ByteArray>();
     bt->writeFuint8(magic_);
     bt->writeFuint8(version_);
     bt->writeFuint8(type_);
+    bt->writeFuint32(data_.size());
     bt->writeStringF32(data_);
     bt->setPosition(0);
     return bt;
   }
 
-  void DecodeMeta(ByteArray::Ptr bt) {
+  void DecodeMeta(const ByteArray::Ptr& bt) {
     magic_ = bt->readFuint8();
     version_ = bt->readFuint8();
     type_ = bt->readFuint8();
     content_length_ = bt->readFuint32();
   }
 
-  void Decode(ByteArray::Ptr bt) {
+  void Decode(const ByteArray::Ptr& bt) override {
     magic_ = bt->readFuint8();
     version_ = bt->readFuint8();
     type_ = bt->readFuint8();
+    content_length_ = bt->readFuint32();
     data_ = bt->readStringF32();
-    content_length_ = data_.size();
+//    content_length_ = data_.size();
   }
 
   std::string ToString() {
@@ -98,9 +115,10 @@ class Protocol {
   }
 
  private:
+  // We called following variables Header except for the data_ field.
   uint8_t magic_ { kMagic };
   uint8_t version_ { kDefaultVersion };
-  uint8_t type_ {static_cast<uint8_t>(MsgType::kNormal)};
+  uint8_t type_ { static_cast<uint8_t>(MsgType::kNormal) };
   uint32_t content_length_ { 0 };
   std::string data_;
 };
