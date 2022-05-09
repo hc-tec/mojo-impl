30aa5f83a2fab5876f0a985f272d9be963a09fca
hc-tec <2173324540@qq.com>
2022-05-09 22:56:08 +0800

    feat: add node

---
+++ src/core/ports/node.cc
@@ -0,0 +1,146 @@
+//
+// Created by titto on 2022/5/8.
+//
+
+#include "node.h"
+
+#include "base/mutex.h"
+#include "base/rand_util.h"
+#include "base/noncopyable.h"
+
+namespace tit {
+namespace mojo {
+namespace ports {
+
+constexpr size_t kRandomNameCacheSize = 256;
+
+class RandomNameGenerator {
+ public:
+  RandomNameGenerator() = default;
+  ~RandomNameGenerator() = default;
+
+  PortName GenerateRandomPortName() {
+    base::MutexLockGuard lock(lock_);
+    if (cache_index_ == kRandomNameCacheSize) {
+      base::RandBytes(cache_, sizeof(PortName) * kRandomNameCacheSize);
+      cache_index_ = 0;
+    }
+    return cache_[cache_index_++];
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(RandomNameGenerator);
+ private:
+  base::MutexLock lock_;
+  PortName cache_[kRandomNameCacheSize];
+  size_t cache_index_ = kRandomNameCacheSize;
+};
+
+RandomNameGenerator g_name_generator;
+
+void GenerateRandomPortName(PortName* name) {
+  *name = g_name_generator.GenerateRandomPortName();
+}
+
+Node::DelegateHolder::DelegateHolder(
+    Node *node,
+    Node::Delegate *delegate)
+    : node_(node),
+      delegate_(delegate) {}
+
+Node::DelegateHolder::~DelegateHolder() {
+  node_ = nullptr;
+  delegate_ = nullptr;
+}
+
+Node::Node(const NodeName &name, Node::Delegate *delegate)
+    : name_(name),
+      delegate_(delegate) {}
+
+Node::~Node() {
+  delegate_ = nullptr;
+}
+
+int Node::SendUserMessageInternal(const PortRef& port_ref,
+                                  const UserMessageEvent::Ptr& message) {
+//  UserMessageEvent::Ptr msg = UserMessageEvent::Ptr();
+  message->set_from_port(port_ref.name());
+  message->set_port_name(port_ref.port()->peer_port_name);
+  delegate_->ForwardEvent(port_ref.port()->peer_node_name,
+                          (const Event&)message);
+}
+
+int Node::CreateUninitializedPort(PortRef* port_ref) {
+  PortName port_name;
+  GenerateRandomPortName(&port_name);
+  Port::Ptr port = Port::Create();
+  int r = AddPortWithName(port_name, port);
+  if (r != OK)
+    return r;
+  *port_ref = PortRef(port_name, port);
+  return OK;
+}
+
+int Node::AddPortWithName(const PortName& port_name,
+                          const Port::Ptr& port) {
+  base::MutexLockGuard g(ports_lock_);
+  if (port->peer_port_name != kInvalidPortName) {
+    peer_port_maps_[port->peer_node_name][port->peer_port_name].emplace(
+      port_name, PortRef(port_name, port)
+    );
+  }
+  if (!ports_.emplace(port_name, port).second) {
+    return ERROR_PORT_EXISTS;
+  }
+  LOG(DEBUG) << "Create port name @" << port_name;
+  return OK;
+}
+
+int Node::InitializePort(const PortRef& port_ref,
+                         const NodeName& peer_node_name,
+                         const PortName& peer_port_name) {
+  base::MutexLockGuard g(ports_lock_);
+  auto port = port_ref.port();
+  if (port->state != Port::State::kUninitialized) {
+    return ERROR_PORT_STATE_UNEXPECTED;
+  }
+  port->state = Port::State::kReceiving;
+  UpdatePortPeerAddress(port_ref.name(),
+                        port,
+                        peer_node_name,
+                        peer_port_name);
+  return OK;
+}
+
+void Node::UpdatePortPeerAddress(const PortName& local_port_name,
+                                 const Port::Ptr& local_port,
+                                 const NodeName& new_peer_node,
+                                 const PortName& new_peer_port) {
+  local_port->peer_node_name = new_peer_node;
+  local_port->peer_port_name = new_peer_port;
+  if (new_peer_port != ports::kInvalidPortName) {
+    peer_port_maps_[new_peer_node][new_peer_port].emplace(
+        local_port_name,
+        PortRef(local_port_name, local_port));
+  }
+}
+
+int Node::CreatePortPair(PortRef* port0_ref, PortRef* port1_ref) {
+  int r = CreateUninitializedPort(port0_ref);
+  if (r != OK)
+    return r;
+  r = CreateUninitializedPort(port1_ref);
+  if (r != OK)
+    return r;
+  r = InitializePort(*port0_ref, name_, port1_ref->name());
+  if (r != OK)
+    return r;
+  r = InitializePort(*port1_ref, name_, port0_ref->name());
+  if (r != OK)
+    return r;
+  return OK;
+}
+
+}  // namespace ports
+
+}  // namespace mojo
+}  // namespace tit
---
+++ src/core/ports/node.h
@@ -0,0 +1,114 @@
+//
+// Created by titto on 2022/5/8.
+//
+
+#ifndef MOJO_IMPL_NODE_H
+#define MOJO_IMPL_NODE_H
+
+#include <memory>
+#include <unordered_map>
+
+#include "base/mutex.h"
+#include "core/ports/name.h"
+#include "core/ports/port.h"
+#include "core/ports/port_ref.h"
+
+namespace tit {
+namespace mojo {
+namespace ports {
+
+enum : int8 {
+  OK = 0,
+  ERROR_PORT_UNKNOWN = -10,
+  ERROR_PORT_EXISTS = -11,
+  ERROR_PORT_STATE_UNEXPECTED = -12,
+  ERROR_PORT_CANNOT_SEND_SELF = -13,
+  ERROR_PORT_PEER_CLOSED = -14,
+  ERROR_PORT_CANNOT_SEND_PEER = -15,
+  ERROR_NOT_IMPLEMENTED = -100,
+};
+
+class Node {
+ public:
+  using Ptr = std::unique_ptr<Node>;
+
+  class Delegate {
+   public:
+    virtual ~Delegate() = default;
+    virtual void ForwardEvent(const NodeName& node, const Event& event) = 0;
+    virtual void BroadcastEvent(const Event& event) = 0;
+    virtual void PortStatusChanged(const PortRef& port_ref) = 0;
+  };
+
+  static Ptr Create() {
+    return std::unique_ptr<Node>();
+  }
+
+  Node(const NodeName& name, Delegate* delegate);
+  ~Node();
+
+  int CreateUninitializedPort(PortRef* port_ref);
+
+  int InitializePort(const PortRef& port_ref,
+                     const NodeName& peer_node_name,
+                     const PortName& peer_port_name);
+
+  int CreatePortPair(PortRef* port0_ref, PortRef* port1_ref);
+
+  int AddPortWithName(const PortName& port_name, const Port::Ptr& port);
+
+  void UpdatePortPeerAddress(const PortName& local_port_name,
+                             const Port::Ptr& local_port,
+                             const NodeName& new_peer_node,
+                             const PortName& new_peer_port);
+
+  int GetMessage(const PortRef& port_ref,
+                 UserMessageEvent::Ptr* message);
+
+  int SendUserMessage(const PortRef& port_ref,
+                      const UserMessageEvent::Ptr& message);
+
+  DISALLOW_COPY_AND_ASSIGN(Node);
+ private:
+  class DelegateHolder {
+   public:
+    DelegateHolder(Node* node, Delegate* delegate);
+
+    DelegateHolder(const DelegateHolder&) = delete;
+    DelegateHolder& operator=(const DelegateHolder&) = delete;
+
+    ~DelegateHolder();
+
+    Delegate* operator->() const {
+      return delegate_;
+    }
+
+   private:
+    Node* node_;
+    Delegate* delegate_;
+  };
+
+  int SendUserMessageInternal(const PortRef& port_ref,
+                              const UserMessageEvent::Ptr& message);
+
+ private:
+  NodeName name_;
+  Delegate* delegate_;
+
+  using LocalPortName = PortName;
+  using PeerPortName = PortName;
+
+  base::MutexLock ports_lock_;
+  std::unordered_map<LocalPortName, Port::Ptr> ports_;
+
+  using PeerPortMap = std::unordered_map<PeerPortName,
+                         std::map<LocalPortName, PortRef>>;
+  std::unordered_map<NodeName, PeerPortMap> peer_port_maps_;
+};
+
+}  // namespace ports
+}  // namespace mojo
+}  // namespace tit
+
+
+#endif  // MOJO_IMPL_NODE_H
