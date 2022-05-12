0ee5c6da74e256b7f5a6e21e4ef1d600d7dbcd3d
hc-tec <2173324540@qq.com>
2022-05-12 13:28:11 +0800

    refactor: modify fields of user message protocol

--- src/core/protocols/user_message.h
+++ src/core/protocols/user_message.h
@@ -21,34 +21,27 @@ class UserMessage : public ProtocolInterface {
     return std::make_shared<UserMessage>();
   }
 
-  static Ptr Create(const ports::PortName& port_name, const std::string& data) {
-    Ptr message = std::make_shared<UserMessage>();
-    message->set_port_name(port_name);
-    message->set_data(data);
-    return message;
-  }
-
-  ports::PortName port_name() const { return port_name_; }
-  std::string data() const { return data_; }
-  MsgType msg_type() const { return static_cast<MsgType>(msg_type_); }
-
-  void set_port_name(const ports::PortName& port_name) {
-    port_name_ = port_name;
-  }
-
-  void set_data(const std::string& data) {
-    data_ = data;
-  }
-
-  void set_msg_type(uint8 msg_type) {
-    msg_type_ = msg_type;
-  }
+//  ports::PortName from_port_name() const { return from_port_name_; }
+//  ports::PortName to_port_name() const { return to_port_name_; }
+//  std::string data() const { return data_; }
+//
+//  void set_from_port_name(const ports::PortName& port_name) {
+//    from_port_name_ = port_name;
+//  }
+//  void set_to_port_name(const ports::PortName& port_name) {
+//    to_port_name_ = port_name;
+//  }
+//  void set_data(const std::string& data) {
+//    data_ = data;
+//  }
 
   // ProtocolInterface
   ByteArray::Ptr Encode() override {
     ByteArray::Ptr bt = std::make_shared<ByteArray>();
-    bt->writeFuint64(port_name_.v1);
-    bt->writeFuint64(port_name_.v2);
+    bt->writeFuint64(from_port_name_.v1);
+    bt->writeFuint64(from_port_name_.v2);
+    bt->writeFuint64(to_port_name_.v1);
+    bt->writeFuint64(to_port_name_.v2);
     bt->writeStringF32(data_);
     bt->setPosition(0);
     return bt;
@@ -57,25 +50,30 @@ class UserMessage : public ProtocolInterface {
     uint64_t v1, v2;
     v1 = bt->readFuint64();
     v2 = bt->readFuint64();
-    port_name_ = ports::PortName(v1, v2);
+    from_port_name_ = ports::PortName(v1, v2);
+    v1 = bt->readFuint64();
+    v2 = bt->readFuint64();
+    to_port_name_ = ports::PortName(v1, v2);
     data_ = bt->readStringF32();
   }
-  MsgType type() override { return MsgType::kEventMessage; }
+
+  MsgType type() override { return MsgType::kUserMessage; }
   ProtocolInterface::Ptr next_layer() override {
     return nullptr;
   }
 
   std::string ToString() {
     std::stringstream ss;
-    ss << "[ port_name_: " << port_name_
+    ss << "[ from_port_name: " << from_port_name_
+       << "[ to_port_name: " << to_port_name_
        << " data=" << data_
        << "]";
     return ss.str();
   }
 
- private:
-  ports::PortName port_name_ { ports::kInvalidPortName };
-  uint8 msg_type_;
+ public:
+  ports::PortName from_port_name_ { ports::kInvalidPortName };
+  ports::PortName to_port_name_ { ports::kInvalidPortName };
   std::string data_;
 };
 
